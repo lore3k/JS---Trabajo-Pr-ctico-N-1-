b61f2e8e5b694486b5922ae7a6f08f9a38dde14e
Lore Ginard <lorenaginard@hotmail.com>
2023-03-01 21:42:48 -0300

    Ejercicio 8 terminado

---
+++ js/ejercicio8.js
@@ -0,0 +1,12 @@
+/*
+8.- Escribe un programa que pida un número y diga si es divisible por 2
+*/
+
+let a = parseInt(prompt("Ingresa un número"));
+
+if((a%2)==0){
+    document.write("Es divisible en 2")
+}
+else{
+    document.write("No es divisible en 2")
+}
