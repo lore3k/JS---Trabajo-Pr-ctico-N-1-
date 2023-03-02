e2321b0ee925b0a0dc9b1001fed1af8c096f2e36
Lore Ginard <lorenaginard@hotmail.com>
2023-03-01 22:28:27 -0300

    Corrección sintaxis

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -5,7 +5,7 @@ es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cu
 
 let a = parseInt(prompt("Ingresa un número"));
 
-if((a%2==0)||(a%3==0)||(a%5==0)||(a%7==0)){
+if((a%2===0)||(a%3===0)||(a%5===0)||(a%7===0)){
     document.write("Es divisible en 2, 3, 5 o 7");
 }
 else{
