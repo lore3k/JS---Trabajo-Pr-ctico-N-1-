4609fb5567f214764b48c75c52dbcc767300b780
Lore Ginard <lorenaginard@hotmail.com>
2023-03-01 22:57:41 -0300

    Ejercicio 9 terminado

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -3,3 +3,16 @@
 Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
 */
 
+let frase = prompt("Escribe una frase");
+let longFrase = frase.length;
+let i;
+
+for(i=0; i<longFrase; i++){
+    if(frase.charAt(i) === "a" || 
+    frase.charAt(i) === "e" || 
+    frase.charAt(i) === "i" || 
+    frase.charAt(i) === "o" || 
+    frase.charAt(i) === "u"){
+        document.write(frase.charAt(i));
+    }
+}
